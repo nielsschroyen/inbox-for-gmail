abbcdc370c60a72705c61ad6d088d3d913cff217
Niels Schroyen <niels.schroyen@gmail.com>
2018-09-27 21:38:08 +0200

    cleanup

--- build/inbox-for-gmail.user.js
+++ build/inbox-for-gmail.user.js
@@ -7,7 +7,7 @@
 // @author      Niels Schroyen
 // @copyright   2018+, Niels Schroyen (https://github.com/nielsschroyen/inbox-for-gmail)
 // @license     MIT; https://raw.githubusercontent.com/nielsschroyen/inbox-for-gmail/master/LICENSE
-// @version     1.0.0
+// @version     1.0.1
 // @icon        https://github.com/nielsschroyen/inbox-for-gmail/master/icon.png
 // @homepageURL https://github.com/nielsschroyen/inbox-for-gmail
 // @supportURL  https://github.com/nielsschroyen/inbox-for-gmail
@@ -145,4 +145,4 @@ var styleSheet = "" +
  "}" +
 "}" +
 "";
-(function() {  'use strict';   //Set stylesheet  var sunAdded = false;  var s = document.createElement('style');  s.type = "text/css";  s.innerHTML = styleSheet;  (document.head || document.documentElement).appendChild(s);  var myDoc = document;  window.setInterval(function(){      var bla = myDoc.querySelectorAll('.TD');     if(myDoc.querySelectorAll('.TD').length > 0 && myDoc.querySelectorAll('.sunshine').length == 0 && myDoc.querySelectorAll('.Cp').length > 0){         var div = myDoc.createElement("div");         div.setAttribute('class', 'sunshine');         myDoc.querySelector(".AO div div").appendChild(div);     }      if(myDoc.querySelectorAll('.TD').length == 0 && myDoc.querySelectorAll('.sunshine').length > 0 && myDoc.querySelectorAll('.Cp').length > 0){          console.log( myDoc.querySelectorAll('.TD').length);          var elements = myDoc.getElementsByClassName('sunshine');          while(elements.length > 0){              elements[0].parentNode.removeChild(elements[0]);          }     }}, 500);})();
+(function() {  'use strict';   //Set stylesheet  var s = document.createElement('style');  s.type = "text/css";  s.innerHTML = styleSheet;  (document.head || document.documentElement).appendChild(s);  //Show sun if no messages are present otherwise hide it  var myDoc = document;  window.setInterval(function(){     if(myDoc.querySelectorAll('.TD').length > 0 && myDoc.querySelectorAll('.sunshine').length == 0 && myDoc.querySelectorAll('.Cp').length > 0){         var div = myDoc.createElement("div");         div.setAttribute('class', 'sunshine');         myDoc.querySelector(".AO div div").appendChild(div);     }      if(myDoc.querySelectorAll('.TD').length == 0 && myDoc.querySelectorAll('.sunshine').length > 0 && myDoc.querySelectorAll('.Cp').length > 0){          var elements = myDoc.querySelectorAll('.sunshine');          while(elements.length > 0){              elements[0].parentNode.removeChild(elements[0]);          }     }}, 500);    })();
--- inbox-for-gmail.js
+++ inbox-for-gmail.js
@@ -2,26 +2,26 @@
   'use strict';
 
    //Set stylesheet
-  var sunAdded = false;
   var s = document.createElement('style');
   s.type = "text/css";
   s.innerHTML = styleSheet;
   (document.head || document.documentElement).appendChild(s);
 
+  //Show sun if no messages are present otherwise hide it
   var myDoc = document;
   window.setInterval(function(){
-      var bla = myDoc.querySelectorAll('.TD');
      if(myDoc.querySelectorAll('.TD').length > 0 && myDoc.querySelectorAll('.sunshine').length == 0 && myDoc.querySelectorAll('.Cp').length > 0){
          var div = myDoc.createElement("div");
          div.setAttribute('class', 'sunshine');
          myDoc.querySelector(".AO div div").appendChild(div);
      }
       if(myDoc.querySelectorAll('.TD').length == 0 && myDoc.querySelectorAll('.sunshine').length > 0 && myDoc.querySelectorAll('.Cp').length > 0){
-          console.log( myDoc.querySelectorAll('.TD').length);
-          var elements = myDoc.getElementsByClassName('sunshine');
+          var elements = myDoc.querySelectorAll('.sunshine');
           while(elements.length > 0){
               elements[0].parentNode.removeChild(elements[0]);
           }
      }
 }, 500);
+
+    
 })();
